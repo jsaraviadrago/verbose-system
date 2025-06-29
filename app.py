377ed92ca1707e9bf02744575310b838241ef482
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2025-06-29 17:15:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import altair as alt
 
 
-
 st.markdown("<h1 style='text-align: center; '>Campeonato Apertura Cambridge Lima College 2025</h1>", unsafe_allow_html=True)
 st.divider()
 
