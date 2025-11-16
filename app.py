37561168d372019e02311933cf541162d113a2f4
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2025-11-16 14:15:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -495,8 +495,10 @@ st.divider()
 
 st.markdown("## <span style='color: #ADD8E6;'>Cuartos de final</span>", unsafe_allow_html=True) # Changed to light blue
 
-st.markdown("**Nota**: El partido de Fiorentina contra Chelsea terminó 0-0. El marcador final es el resultado de la tanda de penales
- **Nota**: El partido Liverpool contra Milan terminó 1-1. El marcador final es el resultado de la tanda de penales.")
+st.markdown(""
+
+**Nota**: El partido de Fiorentina contra Chelsea terminó 0-0. El marcador final es el resultado de la tanda de penales
+ **Nota**: El partido Liverpool contra Milan terminó 1-1. El marcador final es el resultado de la tanda de penales."")
 
 
 #Esto fue en el apertura 2025
