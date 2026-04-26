050b061bd54164dd750580f58353d7d707d3c28a
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2026-04-25 19:55:30 -0500

    Getting files in

--- firestore_client.py
+++ firestore_client.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials, firestore
 import pandas as pd
 import streamlit as st
 
+
 # ── Firebase connection ────────────────────────────────────────────────────────
 @st.cache_resource
 def get_db():
